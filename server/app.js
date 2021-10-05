f451b0270d9ba877104ee2a39f546bd6c599f808
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-10-05 23:56:26 +0100

    install graphql packages

--- server/app.js
+++ server/app.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const graphqlHTTP = require("express-graphql");
+
+
+
 const app = express();
 
-app.listen(4000,()=>console.log('hello graphql is here'))
+app.listen(4000, () => console.log("hello graphql is here"));
