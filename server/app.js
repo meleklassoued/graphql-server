2a981b8bda619c6f868638bb64f660a582c07300
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-10-08 00:08:02 +0100

    return the first schema

--- server/app.js
+++ server/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
 const app = express();
 
-app.use("/graphql", graphqlHTTP({}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    graphiql: true,
+    schema,
+  }),
+);
 
 app.listen(4000, () => console.log("hello graphql is here"));
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,18 +1,35 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLString } = graphql;
-
-const RootQuery = new GraphQLObjectType({
-  name: "RootQueryType",
-  fields: () => ({
-    book: {},
-  }),
-});
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const _ = require("lodash");
+const books = [
+  { name: "name of the wind", genre: "Fantasy", id: "1" },
+  { name: "the final Empire ", genre: "Fantasy", id: "2" },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3" },
+];
 
 const BookType = new GraphQLObjectType({
-  name: "book",
+  name: "Book",
   fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
   }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: () => ({
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // code to get data from db /other source
+        return _.find(books, { id: args.id });
+      },
+    },
+  }),
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
