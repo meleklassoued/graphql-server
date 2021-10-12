527cc07be7dfc44d6c0ec4a1e0f2ca29fd940f56
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-10-12 17:41:36 +0100

    connection to the data base mongo db

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 const app = express();
 
+mongoose.connect(
+  "mongodb+srv://meleklassoued:UMBGlNzzSHgkUW6u@graphql.b09af.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to the data base");
+});
 app.use(
   "/graphql",
   graphqlHTTP({
