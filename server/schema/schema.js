84fb37638d9a6414258f54d9bfef329e77e11680
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-10-12 19:33:32 +0100

    fix the the mistake in the mutation book

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -118,6 +118,22 @@ const Mutations = new GraphQLObjectType({
         return author.save();
       },
     },
+    addBook: {
+      type: BookType,
+      args: {
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString },
+        authorId: { type: GraphQLID },
+      },
+      resolve(parent, args) {
+        let book = new BookSchema({
+          name: args.name,
+          genre: args.genre,
+          authorId: args.authorId,
+        });
+        return book.save();
+      },
+    },
   },
 });
 
