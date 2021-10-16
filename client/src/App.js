411c1cfb0a59fa7255abd0f3e469956c55a5bef1
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-10-16 18:24:26 +0100

    create our first Book component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
+import React from "react";
+/* -------------------------------------------------------------------------- */
+/*                              import components                             */
+/* -------------------------------------------------------------------------- */
+import Booklist from "./components/Booklist";
 
-import './App.css';
+/* -------------------------------------------------------------------------- */
+/*                              import components                             */
+/* -------------------------------------------------------------------------- */
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-    
+    <div className='App'>
+      <h1> Melek's Reading List</h1>
+      <Booklist />
     </div>
   );
 }
--- client/src/components/Booklist.js
+++ client/src/components/Booklist.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+
+
+
 function Booklist() {
   return (
     <div>
