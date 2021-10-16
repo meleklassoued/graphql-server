18a72bee5e90eadab5a2d246f1523c9a171eab7f
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-10-16 18:19:56 +0100

    create our first compoenent

---
+++ client/src/components/Booklist.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Booklist() {
+  return (
+    <div>
+      <ul id='book-list'>
+        <li>Book Name</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Booklist;
