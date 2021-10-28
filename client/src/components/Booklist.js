54a1e9e10aee4ab22ccf449cd3d19be2f1c93b4b
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-10-28 01:50:01 +0100

    Booklist onclick event to display the detailBook

--- client/src/components/Booklist.js
+++ client/src/components/Booklist.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
+import { useState } from "react";
 /* ------------------------------ import QUERY ------------------------------ */
 import { getBooksList } from "../graphql/Queries/query";
+import BookDetail from "./BookDetail";
 /* ------------------------------ finish import QUERY ------------------------------ */
 
 function Booklist() {
+  const [Selected, setSelected] = useState(null);
   const { loading, error, data } = useQuery(getBooksList);
   if (loading) return "loading";
   if (error) return `Error ${error.message}`;
@@ -13,9 +16,16 @@ function Booklist() {
     <div>
       <ul id='book-list'>
         {data.books.map((book) => (
-          <li>{book.name}</li>
+          <li
+            key={book.id}
+            onClick={(e) => {
+              setSelected(book.id);
+            }}>
+            {book.name}
+          </li>
         ))}
       </ul>
+      <BookDetail BookId={Selected}></BookDetail>
     </div>
   );
 }
